7c3d475833fd55dfe784dcf1b3ccdd2643f784f6
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-06 17:40:33 -0200

    visible

--- src/playground/visible.js
+++ src/playground/visible.js
@@ -1,26 +1,42 @@
-let visibility = false;
 
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
-};
 
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? 'Hide details' : 'Show details'}
-      </button>
-      {visibility && (
-        <div>
-          <p>Hey. These are some details you can now see!</p>
-        </div>
-      )}
-    </div>
-  );
 
-  ReactDOM.render(jsx, document.getElementById('app'));
-};
 
-render();
+class VisibilityToggle extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      visibility: false
+    };
+
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+  }
+  handleToggleVisibility(){
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide details' : 'Show details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Hey. These are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
+
+
+
