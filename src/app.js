146a84f44ab6ab784ad723a9b4b2c6d7b1986118
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-08 03:11:40 -0200

    34k15

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
-import './utils.js'
+//import './utils.js'
+import { quare, adds } from './utils.js';
+import { isAdult, canDrink } from './person.js';
 
 console.log('app.js is running!!!!');
+console.log(isAdult(18));
+console.log(canDrink(18));
---
+++ src/person.js
@@ -0,0 +1,24 @@
+
+
+    const isAdult = (age) => {
+        if(age >= 18){
+            return true
+        }else{
+           return false
+        }
+    };
+
+    const canDrink = (age) => {
+        if(age >= 21){
+            return true
+        }else{
+            return false
+        };
+    };
+
+
+
+    export { 
+        isAdult, canDrink
+    };
+
