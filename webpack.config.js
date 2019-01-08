612b029274ca06c8c6a9912cb10593092ff25c0d
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-07 23:06:09 -0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//entry --> output
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'production'
+};
