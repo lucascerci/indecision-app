94bf03efd721dbcf3b37ba43cdc82926e3240fa5
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-08 23:26:35 -0200

    normalizeCSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
