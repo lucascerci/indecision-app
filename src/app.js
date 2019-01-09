cc735522d7cc6a9e5023ae615e529dca74915b0d
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-01-08 23:13:26 -0200

    startStyling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import IndecisionApp from './components/IndecisionApp';
-
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
   
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      }, {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
